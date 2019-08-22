0ba792fd644f9194ff5aef824d6037feace0f385
Amritpal Gidda <amritpals1@hotmail.com>
2019-08-22 16:48:26 -0700

    Parse text to int in edit text

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -14,14 +14,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button addBtn = (Button) findViewById(R.id.addBtn);
-        addBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
+        Button addBtn = (Button) findViewById(R.id.addBtn);         //Button reference
+        addBtn.setOnClickListener(new View.OnClickListener() {      //Anytime button is clicked
+            @Override                                               //-onClick function will run
             public void onClick(View v) {
+                //Retrieve numbers entered in EditText boxes
                 EditText firstNumbEditText = (EditText) findViewById(R.id.firstNumbEditText);
                 EditText secondNumbEditText = (EditText) findViewById(R.id.secondNumbEditText);
-                EditText ThridNumbEditText = (EditText) findViewById(R.id.thirdNumbEditText);
+                EditText thridNumbEditText = (EditText) findViewById(R.id.thirdNumbEditText);
                 TextView resultTextView = (TextView) findViewById(R.id.resultTextView);
+
+                /*
+                Get int value within EditText boxes
+                Go from 'text' to 'string' so that parseInt can convert to int
+                */
+                int num1 = Integer.parseInt(firstNumbEditText.getText().toString());
+                int num2 = Integer.parseInt(secondNumbEditText.getText().toString());
+                int num3 = Integer.parseInt(thridNumbEditText.getText().toString());
+
+                int result = num1 + num2 + num3;
+                //2905
+
             }
         });
     }
