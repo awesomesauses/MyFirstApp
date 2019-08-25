9e475025fe71abace7677f99e41ff16d21442190
Amritpal Gidda <amritpals1@hotmail.com>
2019-08-25 13:40:33 -0700

    Added Subtract and multiply function

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -15,7 +15,57 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button addBtn = (Button) findViewById(R.id.addBtn);         //Button reference
-        addBtn.setOnClickListener(new View.OnClickListener() {      //Anytime button is clicked
+        Button subBtn = (Button) findViewById(R.id.subBtn);
+        Button multBtn = (Button) findViewById(R.id.multBtn);
+
+
+        addBtn.setOnClickListener(new View.OnClickListener() {      //Add button function
+            @Override                                               //Anytime button is clicked
+            public void onClick(View v) {                           //-onClick function will run
+                //Retrieve numbers entered in EditText boxes
+                EditText firstNumbEditText = (EditText) findViewById(R.id.firstNumbEditText);
+                EditText secondNumbEditText = (EditText) findViewById(R.id.secondNumbEditText);
+                EditText thridNumbEditText = (EditText) findViewById(R.id.thirdNumbEditText);
+                TextView resultTextView = (TextView) findViewById(R.id.resultTextView);
+
+                /*
+                Get int value within EditText boxes
+                Go from 'text' to 'string' so that parseInt can convert to int
+                */
+                int num1 = Integer.parseInt(firstNumbEditText.getText().toString());
+                int num2 = Integer.parseInt(secondNumbEditText.getText().toString());
+                int num3 = Integer.parseInt(thridNumbEditText.getText().toString());
+
+                int result = num1 + num2 + num3;
+                resultTextView.setText(result + "");        //Output value to result view
+
+            }
+        });
+
+        subBtn.setOnClickListener(new View.OnClickListener() {      //Subtract Button
+            @Override
+            public void onClick(View v) {
+                //Retrieve numbers entered in EditText boxes
+                EditText firstNumbEditText = (EditText) findViewById(R.id.firstNumbEditText);
+                EditText secondNumbEditText = (EditText) findViewById(R.id.secondNumbEditText);
+                EditText thridNumbEditText = (EditText) findViewById(R.id.thirdNumbEditText);
+                TextView resultTextView = (TextView) findViewById(R.id.resultTextView);
+
+                /*
+                Get int value within EditText boxes
+                Go from 'text' to 'string' so that parseInt can convert to int
+                */
+                int num1 = Integer.parseInt(firstNumbEditText.getText().toString());
+                int num2 = Integer.parseInt(secondNumbEditText.getText().toString());
+                int num3 = Integer.parseInt(thridNumbEditText.getText().toString());
+
+                int result = num1 - num2 - num3;
+                resultTextView.setText(result + "");        //Output value to result view
+
+            }
+        });
+
+        multBtn.setOnClickListener(new View.OnClickListener() {      //Multiply Button function
             @Override                                               //-onClick function will run
             public void onClick(View v) {
                 //Retrieve numbers entered in EditText boxes
@@ -32,11 +82,12 @@ public class MainActivity extends AppCompatActivity {
                 int num2 = Integer.parseInt(secondNumbEditText.getText().toString());
                 int num3 = Integer.parseInt(thridNumbEditText.getText().toString());
 
-                int result = num1 + num2 + num3;
-                resultTextView.setText(result + "");
+                int result = num1 * num2 * num3;
+                resultTextView.setText(result + "");        //Output value to result view
 
-                //2905
             }
         });
+
+
     }
 }
